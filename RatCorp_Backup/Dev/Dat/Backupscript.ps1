a3eb9ea0ea7ebc182ffa3e1594bcddea1384e0c2
ringu123 <42140360+ringu123@users.noreply.github.com>
2020-01-01 17:24:12 +0100

    RatCorp_Backup
    added RatCorp_Backup

---
+++ RatCorp_Backup/Dev/Dat/Backupscript.ps1
@@ -0,0 +1,188 @@
+﻿Function Get-FolderName($initialDirector) {
+    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
+
+    $OpenFolderDialog = New-Object System.Windows.Forms.FolderBrowserDialog
+ 
+    $Topmost = New-Object System.Windows.Forms.Form
+    $Topmost.TopMost = $True
+    $Topmost.MinimizeBox = $True
+
+    $OpenFolderDialog.RootFolder = $initialDirectory
+    $OpenFolderDialog.ShowDialog($Topmost) | Out-Null
+    return $OpenFolderDialog.SelectedPath
+}
+
+
+#Variables, only Change here
+$Destination=Get-FolderName Desktop #Copy the Files to this Location
+$Versions="5" #How many of the last Backups you want to keep
+$BackupDirs=Get-FolderName Desktop #What Folders you want to backup
+ 
+$ExcludeDirs="C:\Users\seimi\OneDrive - Seidl Michael\0-Temp\Dir1","C:\Users\seimi\OneDrive - Seidl Michael\0-Temp\Dir2" #This list of Directories will not be copied
+ 
+$LogName="Log.txt" #Log Name
+$LoggingLevel="3" #LoggingLevel only for Output in Powershell Window, 1=smart, 3=Heavy
+
+ 
+$ExcludeString=""
+[string[]]$excludedArray = $ExcludeDirs -split "," 
+foreach ($Entry in $ExcludeDirs)
+{
+    $Temp="^"+$Entry.Replace("\","\\")
+    $ExcludeString+=$Temp+"|"
+}
+$ExcludeString=$ExcludeString.Substring(0,$ExcludeString.Length-1)
+$ExcludeString
+[RegEx]$exclude = $ExcludeString
+
+#Logging "INFO" "Use orig Backup Dir"
+$Backupdir=$Destination +"\Backup-"+ (Get-Date -format yyyy-MM-dd)+"-"+(Get-Random -Maximum 100000)+"\"
+ 
+ 
+#$BackupdirTemp=$Temp +"\Backup-"+ (Get-Date -format yyyy-MM-dd)+"-"+(Get-Random -Maximum 100000)+"\"
+$Log=$Backupdir+$LogName
+$Log
+$Items=0
+$Count=0
+$ErrorCount=0
+$StartDate=Get-Date #-format dd.MM.yyyy-HH:mm:ss
+ 
+#FUNCTION
+#Logging
+Function Logging ($State, $Message) {
+    $Datum=Get-Date -format dd.MM.yyyy-HH:mm:ss
+ 
+    if (!(Test-Path -Path $Log)) {
+        New-Item -Path $Log -ItemType File | Out-Null
+    }
+    $Text="$Datum - $State"+":"+" $Message"
+ 
+    if ($LoggingLevel -eq "1" -and $Message -notmatch "was copied") {Write-Host $Text}
+    elseif ($LoggingLevel -eq "3") {Write-Host $Text}
+   
+    add-Content -Path $Log -Value $Text
+    
+}
+ 
+ 
+#Create Backupdir
+Function Create-Backupdir {
+    New-Item -Path $Backupdir -ItemType Directory | Out-Null
+    sleep -Seconds 5
+    Logging "INFO" "Create Backupdir $Backupdir"
+}
+ 
+#Delete Backupdir
+Function Delete-Backupdir {
+    $Folder=Get-ChildItem $Destination | where {$_.Attributes -eq "Directory"} | Sort-Object -Property CreationTime -Descending:$false | Select-Object -First 1
+ 
+    Logging "INFO" "Remove Dir: $Folder"
+    
+    $Folder.FullName | Remove-Item -Recurse -Force 
+}
+ 
+ 
+ 
+#Check if Backupdirs and Destination is available
+function Check-Dir {
+    Logging "INFO" "Check if BackupDir and Destination exists"
+    if (!(Test-Path $BackupDirs)) {
+        return $false
+        Logging "Error" "$BackupDirs does not exist"
+    }
+    if (!(Test-Path $Destination)) {
+        return $false
+        Logging "Error" "$Destination does not exist"
+    }
+}
+ 
+#Save all the Files
+Function Make-Backup {
+    Logging "INFO" "Started the Backup"
+    $Files=@()
+    $SumMB=0
+    $SumItems=0
+    $SumCount=0
+    $colItems=0
+    Logging "INFO" "Count all files and create the Top Level Directories"
+ 
+    foreach ($Backup in $BackupDirs) {
+        $colItems = (Get-ChildItem $Backup -recurse | Where-Object {$_.mode -notmatch "h"} | Measure-Object -property length -sum) 
+        $Items=0
+        $FilesCount += Get-ChildItem $Backup -Recurse | Where-Object {$_.mode -notmatch "h"}  
+        Copy-Item -Path $Backup -Destination $Backupdir -Force -ErrorAction SilentlyContinue
+        $SumMB+=$colItems.Sum.ToString()
+        $SumItems+=$colItems.Count
+    }
+ 
+    $TotalMB="{0:N2}" -f ($SumMB / 1MB) + " MB of Files"
+    Logging "INFO" "There are $SumItems Files with  $TotalMB to copy"
+ 
+    foreach ($Backup in $BackupDirs) {
+        $Index=$Backup.LastIndexOf("\")
+        $SplitBackup=$Backup.substring(0,$Index)
+        $Files = Get-ChildItem $Backup -Recurse  | select * | Where-Object {$_.mode -notmatch "h" -and $_.fullname -notmatch $exclude} | select fullname #$_.mode -notmatch "h" -and 
+ 
+        foreach ($File in $Files) {
+            $restpath = $file.fullname.replace($SplitBackup,"")
+            try {
+                Copy-Item  $file.fullname $($Backupdir+$restpath) -Force -ErrorAction SilentlyContinue |Out-Null
+                Logging "INFO" "$file was copied"
+            }
+            catch {
+                $ErrorCount++
+                Logging "ERROR" "$file returned an error an was not copied"
+            }
+            $Items += (Get-item $file.fullname).Length
+            $status = "Copy file {0} of {1} and copied {3} MB of {4} MB: {2}" -f $count,$SumItems,$file.Name,("{0:N2}" -f ($Items / 1MB)).ToString(),("{0:N2}" -f ($SumMB / 1MB)).ToString()
+            $Index=[array]::IndexOf($BackupDirs,$Backup)+1
+            $Text="Copy data Location {0} of {1}" -f $Index ,$BackupDirs.Count
+            Write-Progress -Activity $Text $status -PercentComplete ($Items / $SumMB*100)  
+            if ($File.Attributes -ne "Directory") {$count++}
+        }
+    }
+    $SumCount+=$Count
+    $SumTotalMB="{0:N2}" -f ($Items / 1MB) + " MB of Files"
+    Logging "INFO" "----------------------"
+    Logging "INFO" "Copied $SumCount files with $SumTotalMB"
+    Logging "INFO" "$ErrorCount Files could not be copied"
+
+#Check if Backupdir needs to be cleaned and create Backupdir
+$Count=(Get-ChildItem $Destination | where {$_.Attributes -eq "Directory"}).count
+Logging "INFO" "Check if there are more than $Versions Directories in the Backupdir"
+ 
+if ($count -gt $Versions) 
+{
+ 
+    Delete-Backupdir
+}
+
+ 
+}
+ 
+ 
+#create Backup Dir
+ 
+ 
+ 
+Create-Backupdir
+Logging "INFO" "----------------------"
+Logging "INFO" "Start the Script"
+ 
+#Check if all Dir are existing and do the Backup
+$CheckDir=Check-Dir
+ 
+if ($CheckDir -eq $false) {
+    Logging "ERROR" "One of the Directory are not available, Script has stopped"
+} else {
+    Make-Backup
+ 
+    $Enddate=Get-Date #-format dd.MM.yyyy-HH:mm:ss
+    $span = $EndDate - $StartDate
+    $Minutes=$span.Minutes
+    $Seconds=$Span.Seconds
+ 
+    Logging "INFO" "Backupduration $Minutes Minutes and $Seconds Seconds"
+    Logging "INFO" "----------------------"
+    Logging "INFO" "----------------------"
+}
---
+++ RatCorp_Backup/Dev/Src/backup_v0.7.ps1
@@ -0,0 +1,137 @@
+﻿$logMode = 1;
+
+# log start of script
+$msg = "Running script backup version 0.7"
+log $msg
+
+$backupSrc = testDirectory "C:\Users\vmadmin\Documents\RatCorp_Backup\Dev\Dat\OrdnerStruc";
+$backupDest = testDirectory "C:\Users\vmadmin\Documents\RatCorp_Backup\Dev\Dat\Backup";
+
+$fileCount;
+$directoryCount;
+
+
+$fileCountSrc;
+$directoryCountSrc;
+
+$directoryCountDest=0;
+$fileCountDest=0;
+
+
+$date = Get-Date -Format "MM_dd_yyyy_HH_mm_ss";
+$srcName = Get-Item $backupSrc | Get-ItemPropertyValue -Name Name;
+$backupName = "$srcName-$date"
+
+# create backup folder in the backup directory, named with actual date and the name of the source folder
+$backup = New-Item -Path $backupDest -ItemType "directory" -Name "$backupName"
+
+# copy the files from Source to the backup folder
+makeBackup $backupSrc $backup
+
+# get path where script is run for creation of log file
+$path = Split-Path $script:MyInvocation.MyCommand.Path
+
+# set logFile path and name
+
+$logFile = "$path\"+"$backupName"+".txt"
+
+# Tests if directory exists, if true it returns the path. If false the script will end with an error message.
+function testDirectory($dir){
+if(-not (Test-Path $dir)){
+$msg = "Error: Directory $dir doesn't exist";
+log $msg;
+exit;
+}
+else{
+return $dir;
+}
+}
+
+# Copys all files from the Source folder into the destination folder
+function makeBackup($src, $dest){
+
+    $msg = "Starting backup of $src to $dest"
+
+    log $msg;
+
+    $directories = Get-ChildItem -Path $src -Force -Recurse -Directory
+    $files = Get-ChildItem -Path $src -Force -Recurse -File
+
+    $directoryCountSrc = $directories.Count
+    $msg = "$directoryCountSrc Directories in $src"
+
+        log $msg;
+    
+    $fileCountSrc = $files.Count
+    $msg = "$fileCountSrc Files in $src"
+
+        log $msg;
+
+    copyFiles $src $dest;
+    
+    $msg = "copy of files finished"
+        log $msg
+   
+    $msg = "created $directoryCountDest/$directoryCountSrc directories"
+        log $msg
+
+    $msg = "copied $fileCountDest/$fileCountSrc files"
+        log $msg
+
+    if($directoryCountDest -eq $directoryCountSrc -and $fileCountDest -eq $fileCountSrc){
+        $msg = "Backup SUCCESSFUL, all files have been copied"
+        log $msg
+        }
+        else {
+        $msg = "Backup FAILED, some files may have not been copied"
+        log $msg
+        }
+    }
+
+
+
+# Copys files from one directory to another. If the directory has a subdirectory it creates it and calls this function for the subdirectory.
+
+function copyFiles($src, $dest){
+    Write-Host "Start to copy files in $src";
+    $files = Get-ChildItem -Path $src -Force -File
+
+    if($files){
+    foreach($file in $files){
+    #if($file.Extension -eq ".txt"){
+    Copy-Item $src\$file -Destination $dest
+    $script:fileCountDest++
+    $msg = "Copied $file to $dest"
+    log $msg
+    #}
+    }
+     }
+    
+    $folders = Get-ChildItem -Path $src -Force -Directory
+
+    if($folders){
+
+    foreach($folder in $folders){
+    Copy-Item $src\$folder -Destination $dest
+    $script:directoryCountDest++
+    $msg = "Created directory $folder" 
+    log $msg
+
+
+    copyFiles $src\$folder $dest\$folder
+   }
+  }    
+}
+
+#log 
+Function log ($Message) {    
+    Write-Host "LOGMODE $logMode"
+    $Datum=Get-Date -format dd.MM.yyyy-HH:mm:ss
+    if (!(Test-Path -Path $logFile)-and $logMode -eq 1) {        
+    New-Item -Path $logFile -ItemType File | Out-Null    }    
+    $Text="$Datum"+":"+" $Message"
+    Write-Host $Text
+    if($logMode -eq 1){
+    add-Content -Path $logFile -Value $Text    
+    }
+    }
