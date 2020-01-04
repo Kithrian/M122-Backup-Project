6a30e77ca82375d2d065f1c45bc1f8791c3d0336
Kithrian (CR) <pallene@protonmail.com>
2020-01-04 18:53:07 +0100

    Compressed old files and deleted it

--- RatCorp_Backup/Dev/BackupDataFiles/Old/OrdnerStruktur_FileList.ps1
+++
@@ -1,5 +0,0 @@
-﻿$backupFilesList = "C:\Users\vmadmin\Documents\GitHub\M122-Backup-Project\RatCorp_Backup\Dev\backupFiles.txt"
-
-
-$fileNames = Get-ChildItem -Path  "C:\Users\vmadmin\Documents\GitHub\M122-Backup-Project\RatCorp_Backup\Dev\OrdnerStruc" -Force -Recurse -File | Get-ItemPropertyValue -Name Name | Out-File -FilePath $backupFilesList
-
--- RatCorp_Backup/Dev/BackupDataFiles/Old/RatCorp_FileList.ps1
+++
@@ -1,5 +0,0 @@
-﻿$backupFilesList = "C:\Users\vmadmin\Documents\GitHub\M122-Backup-Project\RatCorp_Backup\Dev\backupFiles_RC.txt"
-
-
-$fileNames = Get-ChildItem -Path  "C:\Users\vmadmin\Documents\GitHub\M122-Backup-Project\RatCorp_Backup\Dev\RatCorp" -Force -Recurse -File | Get-ItemPropertyValue -Name Name | Out-File -FilePath $backupFilesList
-
